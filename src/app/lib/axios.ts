85c3afb22549e588875060983210445df13cbc63
Yernur Melsov <ernurmelsov@gmail.com>
2025-01-21 16:08:30 +0500

    fix

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://spot-sales-prediction-production.up.railway.app'
+  ? 'https://spot-sales-prediction-1.onrender.com'
   : 'http://localhost:8000'
 
 const api = axios.create({
